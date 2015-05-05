25ecbdf8d24400e3181d2db3688f5085d3b42c72
Luis Fernando Chavier <lfchavier@circuitar.com.br>
2015-05-05 16:53:01 -0300

    Use MAC address from EEPROM on WebClient test.

--- examples/WebClient/WebClient.ino
+++ examples/WebClient/WebClient.ino
@@ -13,55 +13,55 @@
  by Tom Igoe, based on work by Adrian McEwen
  modified 12 Aug 2013
  by Soohwan Kim
- 
+ by Luis F. Chavier @ Circuitar Eletronicos
+
  */
 
 #include <SPI.h>
 #include <NanoshieldEthernet.h>
+#include <Wire.h>
+#include <Nanoshield_EEPROM.h>
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-#if defined(WIZ550io_WITH_MACADDRESS) // Use assigned MAC address of WIZ550io
-;
-#else
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-#endif  
+// Set the static IP address to use
+IPAddress ip(192,168,1,177);
+
+// The MAC address will be read from the EEPROM on the Ethernet Nanoshield
+byte mac[6];
+
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
 //IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
 char server[] = "www.google.com";    // name address for Google (using DNS)
-
-// Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192,168,0,177);
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server 
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
 
+// Inialize EEPROM with MAC address
+Nanoshield_EEPROM eeprom(1, 1, 0, true);
+
 void setup() {
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-   while (!Serial) {
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
-  // start the Ethernet connection:
-#if defined(WIZ550io_WITH_MACADDRESS)
-  if (Ethernet.begin() == 0) {
-#else
-  if (Ethernet.begin(mac) == 0) {
-#endif  
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    // try to congifure using IP address instead of DHCP:
-#if defined(WIZ550io_WITH_MACADDRESS)
-    Ethernet.begin(ip);
-#else
-    Ethernet.begin(mac, ip);
-#endif  
+  // Read the MAC address from the Ethernet Nanoshield EEPROM and show it on the terminal
+  eeprom.begin();
+  eeprom.startReading(0x00FA);
+  Serial.println();
+  Serial.print("MAC address: ");
+  for (int i = 0; i < 5; i++) {
+    mac[i] = eeprom.read();
+    Serial.print(mac[i], HEX);
+    Serial.print(":");
   }
-  // give the Ethernet shield a second to initialize:
+  mac[5] = eeprom.read();
+  Serial.println(mac[5], HEX);
+
+  Ethernet.begin(mac, ip);
   delay(1000);
   Serial.println("connecting...");
 
@@ -73,8 +73,7 @@ void setup() {
     client.println("Host: www.google.com");
     client.println("Connection: close");
     client.println();
-  } 
-  else {
+  } else {
     // if you didn't get a connection to the server:
     Serial.println("connection failed");
   }
